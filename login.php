2ec937fdef2f6f6df072ec76c7460cd0fd755f9a
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2025-01-23 12:59:48 +0100

    update form

--- login.php
+++ login.php
@@ -1,17 +1,43 @@
 <?php require "includes/header.php"; ?>
 
+<?php require "config.php"; ?>
+
+<?php
+  if (isset($_POST['submit'])) {
+    if ($_POST['email'] == '' OR $_POST['password'] == '') {
+      echo"some inputs are empty";
+    }else{
+      $email = $_POST["email"];
+      $password = $_POST["password"];
+
+      $login = $conn->query("SELECT * FROM users WHERE email = '$email'");
+      $login->execute();
+      $data = $login->fetch(PDO::FETCH_ASSOC) ;
+      if($login->rowCount() > 0) {
+          if (password_verify($password, $data["mypassword"])) {
+            echo "logged in";
+          }else{
+            echo "email or password is wrong";
+          }
+      }else{
+        echo "email or password is wrong";
+      } 
+      
+    }
+    
+  } 
+
+
+?>
+
 <main class="form-signin w-50 m-auto">
-  <form>
+  <form method="POST" action="login.php">
     <!-- <img class="mb-4 text-center" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
-    <h1 class="h3 mt-5 fw-normal text-center">Please sign in</h1>
+    <h1 class="h3 mt-5 fw-normal text-center">Please log in</h1>
 
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="text" class="form-control" id="floatingInput" placeholder="user.name">
-      <label for="floatingInput">Username</label>
     </div>
     <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
